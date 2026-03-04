16ae7fb64a393ce5117b3d63454f2141803e0336
Mustafa J <mustafaarifin88@admin.sd.belajar.id>
2026-03-04 14:33:10 -0800

    Menjelaskan apa yang diedit hari ini

---
+++ app/Models/SlideShowModel.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class SlideShowModel extends Model
+{
+    protected $table            = 'slide_show';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = ['foto'];
+
+    protected bool $allowEmptyInserts = false;
+
+    protected $useTimestamps = true;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = '';
+}
