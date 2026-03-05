f7856a3e27d648b79d5b079161b6e63838a70f1c
Mustafa Arifin <mustafaarifin88@admin.sd.belajar.id>
2026-03-05 10:44:39 +0700

    Auto Save

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,25 @@
 
 namespace App\Controllers;
 
+use App\Models\SlideShowModel;
+use App\Models\PrestasiModel;
+use App\Models\ProgramModel;
+
 class Home extends BaseController
 {
-    public function index(): string
+    public function index()
     {
-        return view('welcome_message');
+        $slideModel = new SlideShowModel();
+        $prestasiModel = new PrestasiModel();
+        $programModel = new ProgramModel();
+
+        $data = array_merge($this->globalData, [
+            'title'    => 'Beranda',
+            'slides'   => $slideModel->findAll(),
+            'prestasi' => $prestasiModel->orderBy('tgl_penerimaan', 'DESC')->findAll(6),
+            'program'  => $programModel->orderBy('id', 'DESC')->findAll(6)
+        ]);
+
+        return view('frontend/home/index', $data);
     }
 }
