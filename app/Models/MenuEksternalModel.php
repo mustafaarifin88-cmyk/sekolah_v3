a2738ef55151e627e86f8847f65f5eea65870384
Mustafa Arifin <mustafaarifin88@admin.sd.belajar.id>
2026-03-05 09:25:51 +0700

    Auto Save

---
+++ app/Models/MenuEksternalModel.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class MenuEksternalModel extends Model
+{
+    protected $table            = 'menu_eksternal';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = ['judul', 'url'];
+
+    protected bool $allowEmptyInserts = false;
+
+    protected $useTimestamps = true;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = '';
+}
