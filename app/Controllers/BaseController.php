b1a56ca22c09d7545e7af4ed078541ba013b2374
Mustafa Arifin <mustafaarifin88@admin.sd.belajar.id>
2026-03-05 10:43:25 +0700

    Auto Save

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -3,43 +3,32 @@
 namespace App\Controllers;
 
 use CodeIgniter\Controller;
+use CodeIgniter\HTTP\CLIRequest;
+use CodeIgniter\HTTP\IncomingRequest;
 use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\ResponseInterface;
 use Psr\Log\LoggerInterface;
 
-/**
- * BaseController provides a convenient place for loading components
- * and performing functions that are needed by all your controllers.
- *
- * Extend this class in any new controllers:
- * ```
- *     class Home extends BaseController
- * ```
- *
- * For security, be sure to declare any new methods as protected or private.
- */
 abstract class BaseController extends Controller
 {
-    /**
-     * Be sure to declare properties for any property fetch you initialized.
-     * The creation of dynamic property is deprecated in PHP 8.2.
-     */
+    protected $request;
+    protected $helpers = ['form', 'url', 'file'];
+    protected $globalData = [];
 
-    // protected $session;
-
-    /**
-     * @return void
-     */
     public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
     {
-        // Load here all helpers you want to be available in your controllers that extend BaseController.
-        // Caution: Do not put the this below the parent::initController() call below.
-        // $this->helpers = ['form', 'url'];
-
-        // Caution: Do not edit this line.
         parent::initController($request, $response, $logger);
+        
+        $session = \Config\Services::session();
 
-        // Preload any models, libraries, etc, here.
-        // $this->session = service('session');
+        $profilModel = new \App\Models\ProfilSekolahModel();
+        $pengaturanModel = new \App\Models\PengaturanModel();
+        $menuEksternalModel = new \App\Models\MenuEksternalModel();
+
+        $this->globalData = [
+            'profil' => $profilModel->first(),
+            'pengaturan' => $pengaturanModel->first(),
+            'menu_eksternal' => $menuEksternalModel->findAll()
+        ];
     }
 }
