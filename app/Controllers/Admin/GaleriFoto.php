ceccf613e0b81ac33ae4778663cd793420232e58
Mustafa Arifin <mustafaarifin88@admin.sd.belajar.id>
2026-03-05 11:42:50 +0700

    Auto Save

---
+++ app/Controllers/Admin/GaleriFoto.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Controllers\Admin;
+
+use App\Controllers\BaseController;
+use App\Models\GaleriFotoModel;
+
+class GaleriFoto extends BaseController
+{
+    protected $galeriModel;
+
+    public function __construct()
+    {
+        $this->galeriModel = new GaleriFotoModel();
+    }
+
+    public function index()
+    {
+        $data = [
+            'title'  => 'Galeri Foto',
+            'galeri' => $this->galeriModel->getGaleriWithPenulis()
+        ];
+        return view('admin/galeri_foto/index', $data);
+    }
+
+    public function create()
+    {
+        $data = ['title' => 'Tambah Foto'];
+        return view('admin/galeri_foto/create', $data);
+    }
+
+    public function store()
+    {
+        $foto = $this->request->getFile('foto');
+        $namaFoto = $foto->getRandomName();
+        $foto->move('uploads/galeri', $namaFoto);
+
+        $this->galeriModel->insert([
+            'judul'      => $this->request->getPost('judul'),
+            'keterangan' => $this->request->getPost('keterangan'),
+            'foto'       => $namaFoto,
+            'penulis_id' => session()->get('id')
+        ]);
+
+        return redirect()->to('/admin/galeri_foto')->with('success', 'Foto berhasil ditambahkan.');
+    }
+
+    public function edit($id)
+    {
+        $data = [
+            'title'  => 'Edit Foto',
+            'galeri' => $this->galeriModel->find($id)
+        ];
+        return view('admin/galeri_foto/edit', $data);
+    }
+
+    public function update($id)
+    {
+        $galeri = $this->galeriModel->find($id);
+        $dataUpdate = [
+            'judul'      => $this->request->getPost('judul'),
+            'keterangan' => $this->request->getPost('keterangan')
+        ];
+
+        $foto = $this->request->getFile('foto');
+        if ($foto && $foto->isValid() && !$foto->hasMoved()) {
+            $namaFoto = $foto->getRandomName();
+            $foto->move('uploads/galeri', $namaFoto);
+            $dataUpdate['foto'] = $namaFoto;
+            if ($galeri['foto'] && file_exists('uploads/galeri/' . $galeri['foto'])) {
+                unlink('uploads/galeri/' . $galeri['foto']);
+            }
+        }
+
+        $this->galeriModel->update($id, $dataUpdate);
+        return redirect()->to('/admin/galeri_foto')->with('success', 'Foto berhasil diperbarui.');
+    }
+
+    public function delete($id)
+    {
+        $galeri = $this->galeriModel->find($id);
+        if ($galeri['foto'] && file_exists('uploads/galeri/' . $galeri['foto'])) {
+            unlink('uploads/galeri/' . $galeri['foto']);
+        }
+        $this->galeriModel->delete($id);
+        return redirect()->to('/admin/galeri_foto')->with('success', 'Foto berhasil dihapus.');
+    }
+}
